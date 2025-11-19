1feff0df3343d3d7dbf684fbe123b6f27e170408
christel abboud <christel.abboud321@gmail.com>
2025-11-19 08:53:10 +0200

    add all

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 03:38:07 by marvin            #+#    #+#             */
+/*   Updated: 2025/11/16 03:38:07 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int nb, int fd)
+{
+	if (nb == 0)
+		ft_putchar_fd('0', fd);
+	else if (nb == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (nb < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb = -nb;
+		ft_putnbr_fd(nb, fd);
+	}
+	else
+	{
+		if (nb >= 10)
+			ft_putnbr_fd(nb / 10, fd);
+		ft_putchar_fd((nb % 10) + '0', fd);
+	}
+}
